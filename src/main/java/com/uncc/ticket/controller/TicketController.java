24860de3e076786a6a3680ba0ecf3268923cd60d
David Messenger <dpmessen@uncc.edu>
2020-02-19 16:20:27 -0500

    Changes per part 3 of the Lab

--- src/main/java/com/uncc/ticket/controller/TicketController.java
+++ src/main/java/com/uncc/ticket/controller/TicketController.java
@@ -45,14 +45,17 @@ public class TicketController {
         if (bindingResult.hasErrors()) {
             return "tickets/storeTicket";
         };
-
+        ticket.setUsers(usersService.findByEmail(principal.getName()));
+        ticketService.storeTicket(ticket);
         return "redirect:/";
     }
 
     @RequestMapping(value = "/tickets/edit/{id}", method = RequestMethod.GET)
     public String editTicket(Model model,@PathVariable("id") Long id) {
         // Code here
-        return "redirect:/"; //Remove this line
+        model.addAttribute("ticket", ticketService.findById(id));
+        return "tickets/storeTicket";
+        //return "redirect:/"; //Remove this line
     }
 
     @RequestMapping(value = "/tickets/delete/{id}", method = RequestMethod.GET)
